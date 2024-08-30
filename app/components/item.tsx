da6facf67d3f33774dc74c79ed55f3937e489766
Conner Ow <conner@connerow.dev>
2024-08-30 14:04:35 -0500

    fix: small improvements, search

--- app/components/item.tsx
+++ app/components/item.tsx
@@ -2,137 +2,114 @@
 
 import QRCode from "react-qr-code"
 import { Button, Dialog, Text, useToast } from "@fedibtc/ui"
-import { ModContent } from "../page"
 import Flex from "./flex"
 import { useViewport } from "./viewport-provider"
 import { useState } from "react"
 import { styled } from "react-tailwind-variants"
-import * as Tabs from "@radix-ui/react-tabs"
-import sanitizeHtml from "sanitize-html"
-import { marked } from "marked"
+import { Mod } from "../lib/schemas"
 
-const clean = (dirty: string) =>
-  sanitizeHtml(dirty, {
-    allowedTags: [
-      "em",
-      "strong",
-      "a",
-      "code",
-      "br",
-      "s",
-      "strike",
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "blockquote",
-      "ul",
-      "ol",
-      "li",
-      "p",
-      "pre",
-      "img",
-    ],
-    allowedAttributes: {
-      a: ["href", "target"],
-    },
-  })
+const HighlightText = ({
+    content,
+    query,
+}: {
+    content: string
+    query: string
+}) => {
+    if (!query) return <>{content}</>
 
-export default function CatalogItem({ content }: { content: ModContent }) {
-  const [isOpen, setIsOpen] = useState(false)
+    const parts = content.split(new RegExp(`(${query})`, "gi"))
 
-  const { isMobile } = useViewport()
-  const toast = useToast()
+    return (
+        <>
+            {parts.map((part, index) =>
+                part.toLowerCase() === query.toLowerCase() ? (
+                    <strong key={index}>{part}</strong>
+                ) : (
+                    part
+                ),
+            )}
+        </>
+    )
+}
 
-  const handleCopyUrl = () => {
-    navigator.clipboard.writeText(content.meta.url).then(() => {
-      toast.show("Copied to clipboard")
-    })
-  }
+export default function CatalogItem({
+    content,
+    query,
+}: {
+    content: Mod
+    query: string
+}) {
+    const [isOpen, setIsOpen] = useState(false)
 
-  const installContent = (
-    <Flex col gap={2} className="min-w-[320px] shrink-0">
-      <Flex
-        col
-        gap={2}
-        align="center"
-        p={4}
-        className="border border-extraLightGrey rounded-lg"
-      >
-        <QRCode value={content.meta.url} size={256} />
-      </Flex>
-      <Button onClick={handleCopyUrl}>Copy URL</Button>
-    </Flex>
-  )
+    const { isMobile } = useViewport()
+    const toast = useToast()
 
-  const readmeContent = content.readme ? (
-    <Flex grow>
-      <Text
-        className="markdown"
-        dangerouslySetInnerHTML={{
-          __html: clean(marked(content.readme) as string),
-        }}
-      ></Text>
-    </Flex>
-  ) : null
+    const handleCopyUrl = () => {
+        navigator.clipboard.writeText(content.url).then(() => {
+            toast.show("Copied to clipboard")
+        })
+    }
 
-  return (
-    <>
-      <Container onClick={() => setIsOpen(true)}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={content.meta.iconUrl}
-          width={64}
-          height={64}
-          alt={content.meta.name}
-          className="rounded-lg border border-extraLightGrey w-16 h-16"
-        />
-        <Flex col gap={2}>
-          <Text weight="medium">{content.meta.name}</Text>
-          <Text variant="caption">{content.meta.description}</Text>
+    const installContent = (
+        <Flex col gap={2} className="min-w-[320px] shrink-0">
+            <Flex
+                col
+                gap={2}
+                align="center"
+                p={4}
+                className="border border-extraLightGrey rounded-lg"
+            >
+                <QRCode value={content.url} size={256} />
+            </Flex>
+            <Button onClick={handleCopyUrl}>Copy URL</Button>
         </Flex>
-      </Container>
-      <Dialog
-        open={isOpen}
-        onOpenChange={setIsOpen}
-        title={content.meta.name}
-        description={content.meta.description}
-        className="w-screen max-w-[1000px]"
-      >
-        {content.readme && isMobile ? (
-          <Tabs.Root defaultValue="readme">
-            <Tabs.List className="flex gap-2 mb-4">
-              <Tabs.Trigger
-                value="readme"
-                className="grow p-2 flex items-center justify-center text-grey data-[state='active']:text-black data-[state='active']:border-black data-[state='active']:border-solid data-[state='active']:border-0 data-[state='active']:!border-b-2"
-              >
-                About
-              </Tabs.Trigger>
-              <Tabs.Trigger
-                value="install"
-                className="grow p-2 flex items-center justify-center text-grey data-[state='active']:text-black data-[state='active']:border-black data-[state='active']:border-solid data-[state='active']:border-0 data-[state='active']:!border-b-2"
-              >
-                Install
-              </Tabs.Trigger>
-            </Tabs.List>
-            <Tabs.Content value="readme">{readmeContent}</Tabs.Content>
-            <Tabs.Content value="install">{installContent}</Tabs.Content>
-          </Tabs.Root>
-        ) : content.readme && !isMobile ? (
-          <Flex row gap={4} align="start">
-            {readmeContent}
-            {installContent}
-          </Flex>
-        ) : (
-          installContent
-        )}
-      </Dialog>
-    </>
-  )
+    )
+
+    return (
+        <>
+            <Container
+                onClick={async () => {
+                    if (isMobile) {
+                        await navigator.clipboard.writeText(content.url)
+                        toast.show("Copied URL to clipboard")
+
+                        return
+                    }
+
+                    setIsOpen(true)
+                }}
+            >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    src={content.iconUrl}
+                    width={64}
+                    height={64}
+                    alt={content.name}
+                    className="rounded-lg border border-extraLightGrey w-16 h-16"
+                />
+                <Flex col gap={2}>
+                    <Text weight="medium">
+                        <HighlightText content={content.name} query={query} />
+                    </Text>
+                    <Text variant="caption">
+                        <HighlightText content={content.description} query={query} />
+                    </Text>
+                </Flex>
+            </Container>
+            {isMobile ? null : (
+                <Dialog
+                    open={isOpen}
+                    onOpenChange={setIsOpen}
+                    title={content.name}
+                    description={content.description}
+                >
+                    {installContent}
+                </Dialog>
+            )}
+        </>
+    )
 }
 
 const Container = styled("button", {
-  base: "flex gap-4 p-4 rounded-lg border border-extraLightGrey basis-0 grow max-w-[480px] min-w-[320px] cursor-pointer hover:bg-extraLightGrey/25 hover:border-lightGrey border-solid text-left transition-colors",
+    base: "flex gap-4 p-4 rounded-lg border border-extraLightGrey basis-0 grow min-w-[320px] cursor-pointer hover:bg-extraLightGrey/25 hover:border-lightGrey border-solid text-left transition-colors",
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,42 @@
-import { Text } from "@fedibtc/ui"
-import Flex from "./components/flex"
 import path from "path"
 import { Mod, ModGroup, groupSchema } from "./lib/schemas"
 import { readFileSync, readdirSync } from "fs"
-import CatalogItem from "./components/item"
-
-export interface ModContent {
-  meta: Mod
-  readme: string | null
-}
+import PageContent from "./content"
 
 export interface GroupContent {
-  meta: ModGroup
-  mods: Array<ModContent>
+    meta: ModGroup
+    mods: Array<Mod>
 }
 
 export default async function Index() {
-  const groups = readdirSync("mods")
-    .map(dir => {
-      try {
-        const meta = JSON.parse(
-          readFileSync(path.join("mods", dir, "meta.json"), "utf8"),
-        )
-
-        const mods = readdirSync(path.join("mods", dir)).map(mod => {
-          try {
-            const meta = JSON.parse(
-              readFileSync(path.join("mods", dir, mod, "meta.json"), "utf8"),
-            )
-            let readme: null | string = null
-
+    const groups = readdirSync("mods")
+        .map(dir => {
             try {
-              readme = readFileSync(
-                path.join("mods", dir, mod, "README.md"),
-                "utf8",
-              )
-            } catch {}
+                const meta = JSON.parse(
+                    readFileSync(path.join("mods", dir, "meta.json"), "utf8"),
+                )
 
-            return {
-              meta,
-              readme,
+                const mods = readdirSync(path.join("mods", dir)).map(mod => {
+                    try {
+                        const meta = JSON.parse(
+                            readFileSync(path.join("mods", dir, mod, "meta.json"), "utf8"),
+                        )
+
+                        return meta
+                    } catch (e) {
+                        return null
+                    }
+                })
+
+                return {
+                    meta: groupSchema.parse(meta),
+                    mods: mods.filter(Boolean) as Array<Mod>,
+                }
+            } catch (e) {
+                return null
             }
-          } catch (e) {
-            return null
-          }
         })
+        .filter(Boolean) as Array<GroupContent>
 
-        return {
-          meta: groupSchema.parse(meta),
-          mods: mods.filter(Boolean) as Array<ModContent>,
-        }
-      } catch (e) {
-        return null
-      }
-    })
-    .filter(Boolean) as Array<GroupContent>
-
-  return (
-    <Flex col className="w-full items-center" gap={8}>
-      <Flex center p={4}>
-        <Text variant="h1" weight="medium">
-          Fedi Mods Catalog
-        </Text>
-      </Flex>
-
-      {groups.map((group, i) => (
-        <Flex key={i} col gap={4} p={4} width="full" className="max-w-[1200px]">
-          <Text variant="h2" weight="medium">
-            {group.meta.title}
-          </Text>
-          <Flex row gap={2} wrap key={i}>
-            {group.mods.map((mod, j) => (
-              <CatalogItem key={j} content={mod} />
-            ))}
-          </Flex>
-        </Flex>
-      ))}
-    </Flex>
-  )
+    return <PageContent groups={groups} />
 }
