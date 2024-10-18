43a0af7dfc577fbd9cb3d43deac7df17e83e8caf
Conner Ow <conner@connerow.dev>
2024-10-18 03:54:31 -0500

    format

--- app/components/item.tsx
+++ app/components/item.tsx
@@ -9,107 +9,107 @@ import { styled } from "react-tailwind-variants"
 import { Mod } from "../lib/schemas"
 
 const HighlightText = ({
-    content,
-    query,
+  content,
+  query,
 }: {
-    content: string
-    query: string
+  content: string
+  query: string
 }) => {
-    if (!query) return <>{content}</>
+  if (!query) return <>{content}</>
 
-    const parts = content.split(new RegExp(`(${query})`, "gi"))
+  const parts = content.split(new RegExp(`(${query})`, "gi"))
 
-    return (
-        <>
-            {parts.map((part, index) =>
-                part.toLowerCase() === query.toLowerCase() ? (
-                    <strong key={index}>{part}</strong>
-                ) : (
-                    part
-                ),
-            )}
-        </>
-    )
+  return (
+    <>
+      {parts.map((part, index) =>
+        part.toLowerCase() === query.toLowerCase() ? (
+          <strong key={index}>{part}</strong>
+        ) : (
+          part
+        ),
+      )}
+    </>
+  )
 }
 
 export default function CatalogItem({
-    content,
-    query,
+  content,
+  query,
 }: {
-    content: Mod
-    query: string
+  content: Mod
+  query: string
 }) {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
-    const { isMobile } = useViewport()
-    const toast = useToast()
+  const { isMobile } = useViewport()
+  const toast = useToast()
 
-    const handleCopyUrl = () => {
-        navigator.clipboard.writeText(content.url).then(() => {
-            toast.show("Copied to clipboard")
-        })
-    }
+  const handleCopyUrl = () => {
+    navigator.clipboard.writeText(content.url).then(() => {
+      toast.show("Copied to clipboard")
+    })
+  }
 
-    const installContent = (
-        <Flex col gap={2} className="min-w-[320px] shrink-0">
-            <Flex
-                col
-                gap={2}
-                align="center"
-                p={4}
-                className="border border-extraLightGrey rounded-lg"
-            >
-                <QRCode value={content.url} size={256} />
-            </Flex>
-            <Button onClick={handleCopyUrl}>Copy URL</Button>
+  const installContent = (
+    <Flex col gap={2} className="min-w-[320px] shrink-0">
+      <Flex
+        col
+        gap={2}
+        align="center"
+        p={4}
+        className="border border-extraLightGrey rounded-lg"
+      >
+        <QRCode value={content.url} size={256} />
+      </Flex>
+      <Button onClick={handleCopyUrl}>Copy URL</Button>
+    </Flex>
+  )
+
+  return (
+    <>
+      <Container
+        onClick={async () => {
+          if (isMobile) {
+            await navigator.clipboard.writeText(content.url)
+            toast.show("Copied URL to clipboard")
+
+            return
+          }
+
+          setIsOpen(true)
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={content.iconUrl}
+          width={64}
+          height={64}
+          alt={content.name}
+          className="rounded-lg border border-extraLightGrey w-16 h-16"
+        />
+        <Flex col gap={2}>
+          <Text weight="medium">
+            <HighlightText content={content.name} query={query} />
+          </Text>
+          <Text variant="caption">
+            <HighlightText content={content.description} query={query} />
+          </Text>
         </Flex>
-    )
-
-    return (
-        <>
-            <Container
-                onClick={async () => {
-                    if (isMobile) {
-                        await navigator.clipboard.writeText(content.url)
-                        toast.show("Copied URL to clipboard")
-
-                        return
-                    }
-
-                    setIsOpen(true)
-                }}
-            >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    src={content.iconUrl}
-                    width={64}
-                    height={64}
-                    alt={content.name}
-                    className="rounded-lg border border-extraLightGrey w-16 h-16"
-                />
-                <Flex col gap={2}>
-                    <Text weight="medium">
-                        <HighlightText content={content.name} query={query} />
-                    </Text>
-                    <Text variant="caption">
-                        <HighlightText content={content.description} query={query} />
-                    </Text>
-                </Flex>
-            </Container>
-            {isMobile ? null : (
-                <Dialog
-                    open={isOpen}
-                    onOpenChange={setIsOpen}
-                    title={content.name}
-                    description={content.description}
-                >
-                    {installContent}
-                </Dialog>
-            )}
-        </>
-    )
+      </Container>
+      {isMobile ? null : (
+        <Dialog
+          open={isOpen}
+          onOpenChange={setIsOpen}
+          title={content.name}
+          description={content.description}
+        >
+          {installContent}
+        </Dialog>
+      )}
+    </>
+  )
 }
 
 const Container = styled("button", {
-    base: "flex gap-4 p-4 rounded-lg border border-extraLightGrey basis-0 grow min-w-[320px] cursor-pointer hover:bg-extraLightGrey/25 hover:border-lightGrey border-solid text-left transition-colors",
+  base: "flex gap-4 p-4 rounded-lg border border-extraLightGrey basis-0 grow min-w-[320px] cursor-pointer hover:bg-extraLightGrey/25 hover:border-lightGrey border-solid text-left transition-colors",
 })
--- app/content.tsx
+++ app/content.tsx
@@ -8,53 +8,53 @@ import { useEffect, useState } from "react"
 import { Mod } from "./lib/schemas"
 
 export default function PageContent({
-    groups,
+  groups,
 }: {
-    groups: Array<GroupContent>
+  groups: Array<GroupContent>
 }) {
-    const [search, setSearch] = useState("")
-    const [filteredGroups, setFilteredGroups] = useState(groups)
+  const [search, setSearch] = useState("")
+  const [filteredGroups, setFilteredGroups] = useState(groups)
 
-    useEffect(() => {
-        const condition = (mod: Mod) =>
-            new RegExp(search, "gi").test(mod.name) ||
-            new RegExp(search, "gi").test(mod.description)
+  useEffect(() => {
+    const condition = (mod: Mod) =>
+      new RegExp(search, "gi").test(mod.name) ||
+      new RegExp(search, "gi").test(mod.description)
 
-        setFilteredGroups(
-            groups
-                .filter(group => group.mods.some(condition))
-                .map(group => ({
-                    ...group,
-                    mods: group.mods.filter(condition),
-                })),
-        )
-    }, [search, groups])
-
-    return (
-        <Flex col className="w-full items-center" gap={8}>
-            <Flex col gap={4} center p={4} width="full" className="max-w-[480px]">
-                <Text variant="h1" weight="medium">
-                    Fedi Mods Catalog
-                </Text>
-                <Input
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                    placeholder="Search"
-                />
-            </Flex>
-
-            {filteredGroups.map((group, i) => (
-                <Flex key={i} col gap={4} p={4} width="full" className="max-w-[1200px]">
-                    <Text variant="h2" weight="medium">
-                        {group.meta.title}
-                    </Text>
-                    <Flex row gap={2} wrap key={i}>
-                        {group.mods.map((mod, j) => (
-                            <CatalogItem key={j} content={mod} query={search} />
-                        ))}
-                    </Flex>
-                </Flex>
-            ))}
-        </Flex>
+    setFilteredGroups(
+      groups
+        .filter(group => group.mods.some(condition))
+        .map(group => ({
+          ...group,
+          mods: group.mods.filter(condition),
+        })),
     )
+  }, [search, groups])
+
+  return (
+    <Flex col className="w-full items-center" gap={8}>
+      <Flex col gap={4} center p={4} width="full" className="max-w-[480px]">
+        <Text variant="h1" weight="medium">
+          Fedi Mods Catalog
+        </Text>
+        <Input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Search"
+        />
+      </Flex>
+
+      {filteredGroups.map((group, i) => (
+        <Flex key={i} col gap={4} p={4} width="full" className="max-w-[1200px]">
+          <Text variant="h2" weight="medium">
+            {group.meta.title}
+          </Text>
+          <Flex row gap={2} wrap key={i}>
+            {group.mods.map((mod, j) => (
+              <CatalogItem key={j} content={mod} query={search} />
+            ))}
+          </Flex>
+        </Flex>
+      ))}
+    </Flex>
+  )
 }
