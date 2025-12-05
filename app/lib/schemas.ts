6b18779cb78ec422a2cb4c3e87d9bd2b180ef5c5
Evan Martinez <evanm.design@gmail.com>
2025-12-05 17:22:38 -0500

    feat: filter by country and keyword

--- app/lib/schemas.ts
+++ app/lib/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod"
+import { countryCodes } from "./countries"
 
 export const groupSchema = z.object({
   title: z.string(),
@@ -13,6 +14,8 @@ export const modSchema = z.object({
   url: z.string(),
   iconUrl: z.string(),
   description: z.string(),
+  supportedCountryCodes: z.array(z.enum(countryCodes)).optional().default([]),
+  keywords: z.array(z.string()).optional().default([]),
 })
 
 export type Mod = z.infer<typeof modSchema>
