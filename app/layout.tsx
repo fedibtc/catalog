fdb54b95c8e8b148d60efc52e572e05304fddf9a
IroncladDev <conner@ironclad.sh>
2026-01-20 13:57:57 -0600

    chore: update formatter, linter
    Evan, without regard to the many eslint warnings created, continued to
    write some of the most messy code patches I've ever witnessed
    
    This commit adds prettier-eslint and formats the code to use four
    indentation spaces

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,41 +2,41 @@ import { FediInjectionProvider, ToastProvider } from "@fedibtc/ui"
 import "@fedibtc/ui/dist/index.css"
 import type { Metadata } from "next"
 import { Albert_Sans } from "next/font/google"
+import { ViewportProvider } from "./components/viewport-provider"
 import "./globals.css"
 import { fediModName } from "./lib/constants"
-import { ViewportProvider } from "./components/viewport-provider"
 
 const albertSans = Albert_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: fediModName,
-  description: "Find and install your favorite Fedi Mods",
-  icons: ["logo.png"],
+    title: fediModName,
+    description: "Find and install your favorite Fedi Mods",
+    icons: ["logo.png"],
 }
 
 const env = process.env.NEXT_PUBLIC_ENV
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={albertSans.className}>
-        <ToastProvider>
-          <FediInjectionProvider
-            fediModName={fediModName}
-            minSupportedAPIVersion={2}
-            supportedBitcoinNetworks={{
-              signet: env !== "production",
-              bitcoin: env !== "preview",
-            }}
-          >
-            <ViewportProvider>{children}</ViewportProvider>
-          </FediInjectionProvider>
-        </ToastProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={albertSans.className}>
+                <ToastProvider>
+                    <FediInjectionProvider
+                        fediModName={fediModName}
+                        minSupportedAPIVersion={2}
+                        supportedBitcoinNetworks={{
+                            signet: env !== "production",
+                            bitcoin: env !== "preview",
+                        }}
+                    >
+                        <ViewportProvider>{children}</ViewportProvider>
+                    </FediInjectionProvider>
+                </ToastProvider>
+            </body>
+        </html>
+    )
 }
--- app/lib/schemas.ts
+++ app/lib/schemas.ts
@@ -1,24 +1,24 @@
 import { z } from "zod"
-import { countryCodes } from "./countries"
 import { categoryCodes } from "./categories"
+import { countryCodes } from "./countries"
 
 export const groupSchema = z.object({
-  title: z.string(),
-  order: z.number(),
+    title: z.string(),
+    order: z.number(),
 })
 
 export type ModGroup = z.infer<typeof groupSchema>
 
 export const modSchema = z.object({
-  name: z.string(),
-  id: z.string(),
-  url: z.string(),
-  iconUrl: z.string(),
-  description: z.string(),
-  extendedDescription: z.string().optional(),
-  categoryCode: z.enum(categoryCodes),
-  supportedCountryCodes: z.array(z.enum(countryCodes)).optional().default([]),
-  keywords: z.array(z.string()).optional().default([]),
+    name: z.string(),
+    id: z.string(),
+    url: z.string(),
+    iconUrl: z.string(),
+    description: z.string(),
+    extendedDescription: z.string().optional(),
+    categoryCode: z.enum(categoryCodes),
+    supportedCountryCodes: z.array(z.enum(countryCodes)).optional().default([]),
+    keywords: z.array(z.string()).optional().default([]),
 })
 
 export type Mod = z.infer<typeof modSchema>
